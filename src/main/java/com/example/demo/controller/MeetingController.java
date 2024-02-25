7cd3deb10fb2891a8258630101e8a5eb64aab830
LukeyLuke237 <116841654+LukeyLuke237@users.noreply.github.com>
2024-02-25 01:44:44 -0800

    Controller to delete a user''s meeting

--- src/main/java/com/example/demo/controller/MeetingController.java
+++ src/main/java/com/example/demo/controller/MeetingController.java
@@ -1,14 +1,33 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.MeetingRepository;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class MeetingController {
 	
+	@Autowired
+	MeetingRepository meetingRepository;
 	
+	@DeleteMapping("/meetings/{meetingId}")
+	public ResponseEntity<HttpStatus> deleteMeeting(@PathVariable("meetingId") long id) {
+		try {
+			meetingRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 }
