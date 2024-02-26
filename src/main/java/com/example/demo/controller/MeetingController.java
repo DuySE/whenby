0c24d70ec42fcf22154ec6820edf816a2daff8b9
LukeyLuke237 <116841654+LukeyLuke237@users.noreply.github.com>
2024-02-26 03:35:48 -0800

    Endpoint to add a new Meeting

--- src/main/java/com/example/demo/controller/MeetingController.java
+++ src/main/java/com/example/demo/controller/MeetingController.java
@@ -1,15 +1,23 @@
 package com.example.demo.controller;
 
+import java.text.SimpleDateFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Account;
+import com.example.demo.model.AccountRepository;
+import com.example.demo.model.Meeting;
 import com.example.demo.model.MeetingRepository;
+import com.example.demo.request.CreateMeetingRequest;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
@@ -18,6 +26,9 @@ public class MeetingController {
 	
 	@Autowired
 	MeetingRepository meetingRepository;
+	
+	@Autowired
+	AccountRepository accountRepository;
 	
 	@DeleteMapping("/meetings/{meetingId}")
 	public ResponseEntity<HttpStatus> deleteMeeting(@PathVariable("meetingId") long id) {
@@ -29,5 +40,27 @@ public class MeetingController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
 	
+	@PostMapping("/accounts/{userId}/meetings")
+	public ResponseEntity<Meeting> createMeeting(@PathVariable Long userId, 
+											@RequestBody CreateMeetingRequest request){
+		
+		try {
+			Account host = accountRepository.findById(userId).orElse(null);
+			
+			if (host == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+			
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss a");
+            
+			Meeting _meeting = meetingRepository.save(
+					new Meeting(request.getName(), formatter.parse(request.getStartTime()), 
+							formatter.parse(request.getEndTime()), host));
+			return new ResponseEntity<>(_meeting, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
