5b3817479a9b958ea4eb15e05db7215fa261d622
LukeyLuke237 <116841654+LukeyLuke237@users.noreply.github.com>
2024-02-27 22:38:02 -0800

    Added Locale to SimpleDateFormat, and add endpoints to get all meetings and hosted meetings for a user

--- src/main/java/com/example/demo/controller/MeetingController.java
+++ src/main/java/com/example/demo/controller/MeetingController.java
@@ -1,22 +1,31 @@
 package com.example.demo.controller;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 
 import com.example.demo.model.Account;
 import com.example.demo.model.AccountRepository;
 import com.example.demo.model.Meeting;
+import com.example.demo.model.MeetingMember;
 import com.example.demo.model.MeetingRepository;
 import com.example.demo.request.CreateMeetingRequest;
 
@@ -62,7 +71,7 @@ public class MeetingController {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
 			
-			SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss a");
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss a", Locale.ENGLISH);
             
 			Meeting _meeting = meetingRepository.save(
 					new Meeting(request.getName(), formatter.parse(request.getStartTime()), 
@@ -72,4 +81,42 @@ public class MeetingController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+	
+	@GetMapping("/accounts/{userId}/hostedMeetings")
+	public ResponseEntity<List<Meeting>> getMeetingsHostedByUser(@PathVariable Long userId) {
+		
+		List<Meeting> allMeetings = meetingRepository.findAll();
+		List<Meeting> meetingsHostedByUser = allMeetings.stream()
+				.filter(meeting -> meeting.getHost().getId() == userId)
+				.collect(Collectors.toList());
+		
+		if(meetingsHostedByUser.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(meetingsHostedByUser, HttpStatus.OK);
+	}
+	
+	@GetMapping("/accounts/{userId}/joinedMeetings")
+	public ResponseEntity<List<Meeting>> getAllMeetings(@PathVariable Long userId){
+		
+		try {
+			Account user = accountRepository.findById(userId).orElse(null);
+			
+			if(user == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND); 
+			} else {
+				Set<MeetingMember> joinedMeetings = user.getJoinedMeetings();
+				List<Meeting> meetings = joinedMeetings.stream()
+                        .map(MeetingMember::getMeeting)
+                        .collect(Collectors.toList());
+				if (meetings.isEmpty()) {
+					return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+				}
+				return new ResponseEntity<>(meetings, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}		
+	}
+	
 }
